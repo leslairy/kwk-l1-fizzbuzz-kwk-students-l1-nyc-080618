4b103bd706a25a4d85e1b08255ecdd90bad96bd3
Leyla Alekperova <leslairy@gmail.com>
2018-08-07 17:55:42 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
-x=18
-if x%3==0
+x=15
+if x%3==0 && x%5!=0
 print "Fizz"
-elsif x%5==0
+elsif x%5==0 && x%3!=0
 print "Buzz"
-else "true"==(x%5==0 && x%3==0)
+else "true"==(x%5==0 && x%3==0 && x%15==0)
   print "FizzBuzz"
 end
